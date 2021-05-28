48e4fad3604e51e9c523065a6485006d519873c0
chriss1245 <christopher.manzano.vimos@gmail.com>
2021-05-28 22:50:58 +0200

    Solved conflicts with git submodules

---
+++ backend_gameServer.py
@@ -0,0 +1,2 @@
+# Auxiliary method for addapting module name with the naming conventions
+__import__('backend-gameServer')
--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-from gameServer import gameServer
+from backend_gameServer import gameServer
 
 if __name__=='__main__':
     gameServer.run(host='0.0.0.0')
